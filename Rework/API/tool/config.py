bb19a95c5292167d5e3dae4f2d27f91794bf2130
mar <152429982+Rainfue@users.noreply.github.com>
2025-05-13 22:39:31 +0300

    config file deleted for safety

--- Rework/API/tool/config.py
+++
@@ -1,6 +0,0 @@
-TGTOKEN = '6777746191:AAENdHotSulTYwyqg1LtaE3dVCpCQuUjwns'
-YMTOKEN = 'y0_AgAAAABUzxkNAAG8XgAAAAD5FT2pAAANjfSxyG5KUJGWZL4YyRTcLHW4qQ'
-
-MODEL_PATH = 'Model/model_10ep/weights/best.pt'
-USERS_DB = 'API/database/users_data.db'
-TRACKS_DB = 'API/database/new_data.db'
